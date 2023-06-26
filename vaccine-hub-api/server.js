8e059c002a72bebe9c753af44121aaca438f65a8
Kennedy Parks <kennedyjparks@gmail.com>
2023-06-26 16:21:21 -0700

    Added authentication

---
+++ vaccine-hub-api/server.js
@@ -0,0 +1,35 @@
+const express = require("express"); //Import the Express.js framework
+const cors = require("cors"); // Import the CORS middleware
+const morgan = require("morgan"); // Import the Morgan middleware for logging
+
+const { NotFoundError } = require("./utils/errors")
+
+// const carsRoutes = require("./routes/cars"); // Import the cars router module
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors()); // Enable CORS middleware to handle cross-origin requests
+app.use(morgan("dev")); // Use Morgan middleware with 'dev' format for request logging
+app.use(express.json()); // Parse incoming requests with JSON payloads
+
+app.use((req, res, next) => {
+    return next(new NotFoundError())
+})
+
+app.use((req, res, next) => {
+    const status = err.status || 500
+    const message = err.message
+
+    return res.status(status).json({
+        error: { message, status }
+  })
+})
+
+/* //Mount the cars router at the '/api/cars'
+app.use("/api/cars", carsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+*/
